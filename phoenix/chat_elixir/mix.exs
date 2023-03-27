c8f8a8a16a7bcb9653a4bb968b9ae8ddecbccd6d
SetasB <jsetas@bsolus.pt>
2023-03-27 09:27:45 +0100

    :heavy_plus_sign: Add distillery for release

--- phoenix/chat_elixir/mix.exs
+++ phoenix/chat_elixir/mix.exs
@@ -50,7 +50,8 @@ defmodule ChatElixir.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:httpoison, "~> 2.0"}
+      {:httpoison, "~> 2.0"},
+      {:distillery, "~> 2.1"}
     ]
   end
 
