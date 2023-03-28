e1d76b717fac05542c3aea6df5475711dd34db7b
SetasB <jsetas@bsolus.pt>
2023-03-28 10:11:31 +0100

    :sparkles: Add gigalixir deployment

--- phoenix/chat_elixir/config/prod.exs
+++ phoenix/chat_elixir/config/prod.exs
@@ -19,3 +19,20 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+# Gigalixir config
+config :chat_elixir, ChatElixirWeb.Endpoint,
+  server: true, # Without this line, your app will not start the web server!
+  load_from_system_env: true, # Needed for Phoenix 1.3. Doesn't hurt for other versions
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "smartcrop.pt", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  version: Mix.Project.config[:version] # To bust cache during hot upgrades
+
+config :chat_elixir, ChatElixir.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "", # Works around a bug in older versions of ecto. Doesn't hurt for other versions.
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
