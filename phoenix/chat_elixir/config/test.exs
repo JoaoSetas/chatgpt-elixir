99fbf3ba41905fab975a86a8889e612b9f5941ce
SetasB <jsetas@bsolus.pt>
2023-06-21 17:16:33 +0100

    :art: Improve github action

--- phoenix/chat_elixir/config/test.exs
+++ phoenix/chat_elixir/config/test.exs
@@ -12,6 +12,7 @@ config :chat_elixir, ChatElixir.Repo,
   database: "chat_elixir_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
+  socket_options: [:inet6]
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
